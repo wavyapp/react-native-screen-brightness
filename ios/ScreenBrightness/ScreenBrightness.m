499846362d4a36ec64f58a7c42ebc8915e164348
Alex Lévy <alexlevy0@gmail.com>
2019-11-27 11:25:43 +0100

    Update ScreenBrightness.m

--- ios/ScreenBrightness/ScreenBrightness.m
+++ ios/ScreenBrightness/ScreenBrightness.m
@@ -1,22 +1,19 @@
-//
-//  ScreenBrightness.m
-//  ScreenBrightness
-//
-//  Created by Atticus White on 12/7/15.
-//  Copyright © 2015 Atticus White. All rights reserved.
-//
-
 #import "ScreenBrightness.h"
-#if __has_include(<React/RCTBridgeModule.H>)
-#else
-#import "RCTBridgeModule.h"
-#import "RCTBridge.h"
-#import "RCTEventDispatcher.h"
-#endif
 
 @implementation ScreenBrightness
 
 RCT_EXPORT_MODULE();
+
++ (BOOL)requiresMainQueueSetup
+{
+    return NO;
+}
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
 
 RCT_EXPORT_METHOD(getBrightness:(RCTPromiseResolveBlock)resolve
                   getScreenBrightnessRejector:(RCTPromiseRejectBlock)reject) {
