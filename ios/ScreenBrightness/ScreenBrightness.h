7fb21c41e04e9796409c8c8e9542807ecb81195a
Alex Lévy <alexlevy0@gmail.com>
2019-11-27 11:24:40 +0100

    Update ScreenBrightness.h

--- ios/ScreenBrightness/ScreenBrightness.h
+++ ios/ScreenBrightness/ScreenBrightness.h
@@ -1,21 +1,10 @@
-//
-//  ScreenBrightness.h
-//  ScreenBrightness
-//
-//  Created by Atticus White on 12/7/15.
-//  Copyright © 2015 Atticus White. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+
 #if __has_include(<React/RCTBridgeModule.H>)
 #import <React/RCTBridgeModule.h>
-#import <React/RCTBridge.h>
-#import <React/RCTEventDispatcher.h>
 #else
 #import "RCTBridgeModule.h"
-#import "RCTBridge.h"
-#import "RCTEventDispatcher.h"
 #endif
 
 @interface ScreenBrightness : NSObject<RCTBridgeModule>
